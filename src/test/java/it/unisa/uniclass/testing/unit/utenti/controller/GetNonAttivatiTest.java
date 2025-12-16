9d2ec33aa8e9bbff3cea1426fd5fd9492742e207
RedMulaCrackIn <giony2003@gmail.com>
2025-12-16 19:50:44 +0100

    -fix: Remove ServletException handling from GetNonAttivatiTest methods

--- src/test/java/it/unisa/uniclass/testing/unit/utenti/controller/GetNonAttivatiTest.java
+++ src/test/java/it/unisa/uniclass/testing/unit/utenti/controller/GetNonAttivatiTest.java
@@ -4,7 +4,6 @@ import it.unisa.uniclass.utenti.controller.GetNonAttivati;
 import it.unisa.uniclass.utenti.model.Accademico;
 import it.unisa.uniclass.utenti.model.Tipo;
 import it.unisa.uniclass.utenti.service.AccademicoService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,12 +24,12 @@ class GetNonAttivatiTest {
     // Sottoclasse per rendere pubblici i metodi protetti
     static class TestableGetNonAttivati extends GetNonAttivati {
         @Override
-        public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        public void doGet(HttpServletRequest req, HttpServletResponse resp) {
             super.doGet(req, resp);
         }
 
         @Override
-        public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        public void doPost(HttpServletRequest req, HttpServletResponse resp) {
             super.doPost(req, resp);
         }
     }
